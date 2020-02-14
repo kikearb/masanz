343b6bb2d5af25cf60f43915b8f8d157c55b2a44
idoia98 <56587188+idoia98@users.noreply.github.com>
2020-02-14 11:14:38 +0100

    carrusel netflix animadas (#3)
    Co-authored-by: kikearb <kikearbunies@gmail.com>

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,7 +7,21 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <p>Contenido de la página</p>
+      <h1>Series TV</h1>
+    <section>
+      <div>
+        <Image img="https://www.marinerocomunicacion.com/wp-content/uploads/2019/10/cut-horizontal-ADIOS_POSTER_4X2_LOW.jpg" />
+      </div>
+      <div>
+        <Image img="https://3.bp.blogspot.com/-s3c7_9Dwk5g/TYcBz5kKuUI/AAAAAAAAN1s/LGzXdWI7hTc/s1600/thor-2011-carteles-2.jpg" />
+      </div>
+      <div>
+        <Image img="https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/poster-joker-2-1567010576.jpg?crop=1.00xw:0.349xh;0,0.137xh&resize=480:*" />
+      </div>
+      <div>
+        <Image img="https://valenciaplaza.com/public/Image/2016/12/culturplaza_66_01_NoticiaAmpliada.jpg"/>
+      </div>
+    </section>
   </Layout>
 )
 
