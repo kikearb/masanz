e0a19a54fe23310d8417b7e550ae5d4aed0cbf8a
idoia <ipalomabel1@educacion.navarra.es>
2020-02-14 11:04:09 +0100

    carrusel netflix animadas

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,12 +9,21 @@ import * as astronaut from "../assets/images/gatsby-astronaut.png"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div>
-      <Image img={astronaut} alt="Astronauta gatsby" />
-    </div>
+      <h1>Series TV</h1>
+    <section>
+      <div>
+        <Image img="https://www.marinerocomunicacion.com/wp-content/uploads/2019/10/cut-horizontal-ADIOS_POSTER_4X2_LOW.jpg" />
+      </div>
+      <div>
+        <Image img="https://3.bp.blogspot.com/-s3c7_9Dwk5g/TYcBz5kKuUI/AAAAAAAAN1s/LGzXdWI7hTc/s1600/thor-2011-carteles-2.jpg" />
+      </div>
+      <div>
+        <Image img="https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/poster-joker-2-1567010576.jpg?crop=1.00xw:0.349xh;0,0.137xh&resize=480:*" />
+      </div>
+      <div>
+        <Image img="https://valenciaplaza.com/public/Image/2016/12/culturplaza_66_01_NoticiaAmpliada.jpg"/>
+      </div>
+    </section>
   </Layout>
 )
 
