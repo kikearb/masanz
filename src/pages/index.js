79e2034af568984cd07d414448bac9963af0b9ee
Kike Arbunies <kikearbunies@gmail.com>
2020-02-14 12:14:35 +0100

    Corrige import

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,7 @@ import React from "react"
 import "../assets/scss/style.scss"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import Image from '../components/image'
 
 const IndexPage = () => (
   <Layout>
